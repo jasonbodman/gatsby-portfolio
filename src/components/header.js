7dedc29b488d57220a6cc2e4f189bdb6bb0f9184
Jason Bodman <bodmand1@gmail.com>
2019-07-28 09:03:16 -0400

    Update header button

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,31 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = () => <header></header>
+const Header = () => (
+  <header>
+    <div class="nav-contact">
+      <span class="nav-link center">
+        &#9776;<span class="nav-menu"> Menu</span>
+      </span>
+    </div>
+  </header>
+)
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "180px"
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0"
+}
+
+function checkNav() {
+  var x = document.getElementById("mySidenav")
+  if (x.style.width === "0px") {
+    x.style.width = " 180px"
+  } else {
+    x.style.width = "0px"
+  }
+}
 
 export default Header

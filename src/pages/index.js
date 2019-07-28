faa7e56f2a9981dd1c0783d96e6a68493d2dfea6
Jason Bodman <bodmand1@gmail.com>
2019-07-28 15:53:32 -0400

    Update styled components for landing section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,56 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
+import img from "../images/main2.jpg"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi, I'm Jason!</h1>
-    <p>
-      I am a freelance web designer who can help your business succeed by
-      transforming your digital brand and building stronger connections with
-      your customers. Let's get started!
-    </p>
-  </Layout>
+    <Container>
+      <Title>
+        <h1>Hi, I'm Jason!</h1>
+        <p>
+          I am a freelance web designer who can help your business succeed by
+          transforming your digital brand and building stronger connections with
+          your customers. Let's get started!
+        </p>
+      </Title>
+    </Container>
+  </>
 )
+
+const Container = styled.div`
+  background-image: url(${img});
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+`
+
+const Title = styled.div`
+  position: absolute;
+  top: 60%;
+  width: 95%;
+  max-width: 1000px;
+  text-align: left;
+  background-color: #8e4444;
+  color: #fff;
+  padding: 20px 20px 20px 40px;
+  box-shadow: 0px 0px 30px -10px black;
+  margin: auto;
+`
+
+const Section = styled.div`
+  background-image: url(${img});
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+`
 
 export default IndexPage
